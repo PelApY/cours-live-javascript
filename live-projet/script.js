2450774723dae17fd425bd4f3c4ed13ecb24f502
PelApY <anthony.pelong@gmail.com>
2024-06-20 09:01:42 +0200

    init projet

---
+++ live-projet/script.js
@@ -0,0 +1,10 @@
+console.log('V1 : Mon dico anglais')
+/*
+MON PROGRAMME : 
+> Je veux pouvoir donner la définition d'un mot à mes utilisateurs
+- 1. Récupérer le mot saisi par l'utilisateur
+- 2. Envoyer le mot à l'API ( https://dictionaryapi.dev/ )
+- 3. Récupérer le JSON (la donnée) en lien avec mon mot
+- 4. Afficher les informations de mon mot sur ma page (HTML)
+- 5. Ajouter un lecteur pour écouter la prononciation du mot
+*/
